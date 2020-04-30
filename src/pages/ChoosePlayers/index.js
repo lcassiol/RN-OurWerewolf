2d665270f579488e73cc66d82ecdad434f9b4f20
Cassio C <LccassioL@gmail.com>
2020-04-30 16:17:30 -0300

    Add style to chooseplayers screen

--- src/pages/ChoosePlayers/index.js
+++ src/pages/ChoosePlayers/index.js
@@ -1,10 +1,90 @@
 import React from "react";
-import { ImageBackground } from "react-native";
+import { ImageBackground, FlatList } from "react-native";
 
 import background from "../../assets/backgroundwithouttext.png";
-import { Container, Title, Button, TextButton } from "./styles";
+import {
+  Container,
+  AddPlayer,
+  Title,
+  Content,
+  PlayersList,
+  Player,
+  PlayerImage,
+  PlayerName,
+  Button,
+  TextButton,
+} from "./styles";
+
+import DefaultImage from "../../assets/unknown_profile.png";
 
 export default function ChoosePlayers({ navigation }) {
+  const data = [
+    {
+      id: 1,
+      name: "Jubileu",
+    },
+    {
+      id: 2,
+      name: "Javax",
+    },
+    {
+      id: 3,
+      name: "Normax",
+    },
+    {
+      id: 4,
+      name: "Cassio",
+    },
+    {
+      id: 5,
+      name: "OPa",
+    },
+    {
+      id: 6,
+      name: "Joel",
+    },
+    {
+      id: 7,
+      name: "Epa epa",
+    },
+    {
+      id: 8,
+      name: "Uol",
+    },
+    {
+      id: 9,
+      name: "ashu",
+    },
+    {
+      id: 10,
+      name: "Xxz",
+    },
+    {
+      id: 11,
+      name: "weaw",
+    },
+    {
+      id: 12,
+      name: "sklwo",
+    },
+    {
+      id: 13,
+      name: "Uol",
+    },
+    {
+      id: 14,
+      name: "Uol",
+    },
+    {
+      id: 15,
+      name: "EUyt",
+    },
+    {
+      id: 16,
+      name: "0oi0",
+    },
+  ];
+
   return (
     <ImageBackground
       source={background}
@@ -12,7 +92,31 @@ export default function ChoosePlayers({ navigation }) {
       style={{ flex: 1 }}
     >
       <Container>
-        <Title>ChoosePlayers</Title>
+        <AddPlayer>
+          <Title>Adicione players +</Title>
+        </AddPlayer>
+        <Content>
+          <PlayersList
+            data={data}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item) => item.id}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "flex-start",
+            }}
+            renderItem={({ item }) => {
+              return (
+                <Player>
+                  <PlayerImage source={DefaultImage} />
+                  <PlayerName>{item.name}</PlayerName>
+                </Player>
+              );
+            }}
+          />
+        </Content>
+        <Button>
+          <TextButton>PRÓXIMO</TextButton>
+        </Button>
       </Container>
     </ImageBackground>
   );
--- src/pages/ChoosePlayers/styles.js
+++ src/pages/ChoosePlayers/styles.js
@@ -1,21 +1,52 @@
 import styled from "styled-components/native";
+import { FlatList } from "react-native";
 
 export const Container = styled.SafeAreaView`
   flex: 1;
   margin-top: 100px;
 `;
 
+export const AddPlayer = styled.TouchableOpacity`
+  background: #174c66;
+  height: 60px;
+  justify-content: center;
+`;
+
 export const Title = styled.Text`
   font-size: 20px;
-  color: #009999;
+  color: #fff;
+  align-self: flex-start;
+  margin-left: 50px;
+`;
+
+export const Content = styled.View`
+  align-items: center;
+  background: rgba(255, 255, 255, 0.5);
+  flex: 1;
+`;
+
+export const PlayersList = styled(FlatList).attrs({})``;
+
+export const Player = styled.View`
+  align-items: center;
+  margin: 8px 12px;
+`;
+
+export const PlayerImage = styled.Image`
+  width: 85px;
+  height: 85px;
+`;
+
+export const PlayerName = styled.Text`
+  font-weight: bold;
 `;
 
 export const Button = styled.TouchableOpacity`
   background: #174c66;
-  padding: 15px;
-  margin-bottom: 35px;
   border-radius: 5px;
-  align-self: center;
+  align-items: center;
+  justify-content: center;
+  height: 65px;
 `;
 
 export const TextButton = styled.Text`
