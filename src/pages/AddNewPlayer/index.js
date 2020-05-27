474858d65eee8e0d83dd4a8f8c787a4fc137bdeb
Cassio C <LccassioL@gmail.com>
2020-05-27 18:15:04 -0300

    Add camera to new player screen

--- src/pages/AddNewPlayer/index.js
+++ src/pages/AddNewPlayer/index.js
@@ -1,22 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { Camera } from 'expo-camera';
 import { MaterialIcons } from '@expo/vector-icons';
 
 import { ImageBackground, KeyboardAvoidingView } from 'react-native';
-import {
-  View,
-  TextInput,
-  Text,
-  Platform,
-  TouchableWithoutFeedback,
-  Button,
-  Keyboard,
-} from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
 import background from '../../assets/backgroundwithouttext.png';
 import DefaultImage from '../../assets/unknown_profile.png';
 
 import {
-  Container,
   Content,
   PlayerImage,
   ButtonCamera,
@@ -26,6 +18,25 @@ import {
 } from './styles';
 
 export default function AddNewPlayer() {
+  const camera = React.createRef();
+  const [showCamera, setShowCamera] = useState(false);
+  const [hasPermission, setHasPermission] = useState(null);
+  const [type, setType] = useState(Camera.Constants.Type.front);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  async function takePicture() {
+    if (this.camera) {
+      let photo = await camera.takePictureAsync();
+      console.log(photo);
+    }
+  }
+
   return (
     <ImageBackground
       source={background}
@@ -39,14 +50,36 @@ export default function AddNewPlayer() {
           keyboardVerticalOffset="20"
         >
           <View style={{ padding: 24, flex: 1, justifyContent: 'center' }}>
-            <ButtonCamera>
-              <PlayerImage source={DefaultImage} />
+            {showCamera ? (
+              <Camera
+                style={{ width: 250, height: 300, marginTop: -20 }}
+                ref={camera}
+                type={type}
+              />
+            ) : (
+              <ButtonCamera
+                onPress={() => {
+                  setType(
+                    type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                  );
+                }}
+              >
+                <PlayerImage source={DefaultImage} />
+              </ButtonCamera>
+            )}
+
+            <ButtonCamera onPress={takePicture}>
               <MaterialIcons
-                style={{ marginTop: -30 }}
+                style={{ marginTop: -20 }}
                 name="photo-camera"
                 size={45}
                 color="#000"
               />
+              {hasPermission === false && (
+                <Text>Acesso a camera rejeitado</Text>
+              )}
             </ButtonCamera>
 
             <PlayerName placeholder="Nome do jogador(a)" />
